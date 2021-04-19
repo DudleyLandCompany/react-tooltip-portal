34dfd9d32a775fdc546d6193e32b01ba130747b3
Jason Snyder <JasonSnyder@Dudley-land.com>
2021-04-19 09:18:40 -0500

    updating types def

--- index.d.ts
+++ index.d.ts
@@ -1,20 +1,20 @@
 declare module 'react-tooltip-portal' {
-    import {CSSProperties, PureComponent} from 'react';
+  import { CSSProperties, PureComponent } from 'react';
 
-    type Positions = 'top' | 'left' | 'bottom' | 'right';
+  type Positions = 'top' | 'left' | 'bottom' | 'right' | 'bottom left';
 
-    export interface TooltipProps<E extends Element> {
-        active: boolean;
-        parent: E;
-        children: React.ReactNode;
-        offset?: number;
-        position?: Positions;
-        tipStyle?: CSSProperties;
-        hoverEvents?: boolean;
-        timeout?: number;
-        className?: string;
-    }
+  export interface TooltipProps<E extends Element> {
+    active: boolean;
+    parent: E;
+    children: React.ReactNode;
+    offset?: number;
+    position?: Positions;
+    tipStyle?: CSSProperties;
+    hoverEvents?: boolean;
+    timeout?: number;
+    className?: string;
+  }
 
-    class Tooltip<E extends Element> extends PureComponent<TooltipProps<E>> {}
-    export default Tooltip;
+  class Tooltip<E extends Element> extends PureComponent<TooltipProps<E>> {}
+  export default Tooltip;
 }
